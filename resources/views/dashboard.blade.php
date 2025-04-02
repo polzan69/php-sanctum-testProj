1d18cc4f3a6f0563af5a733d6a020c47373ba859
polzan69 <pat2patb@gmail.com>
2025-04-02 15:04:52 +0800

    minor change

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,9 +9,11 @@
         
         @if(session('auth_token'))
             <div class="alert alert-info">
-                <p><strong>Your API Token:</strong></p>
+                <p><strong>Your Generated Token:</strong></p>
                 <p class="text-break">{{ session('auth_token') }}</p>
-                <p class="small">This token would normally be used for API authentication, not displayed on the page.</p>
+                <p class="small">This token would normally be used for API authentication, and not to be displayed on any page.</p>
+                <p class="small">but for this example, it is displayed on the dashboard to ensure that every login generates a new token.</p>
+                <p class="small">To generate a new token, please log out and log back in.</p>
             </div>
         @endif
     </div>
